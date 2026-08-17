665373aee149ce00aa81dc94ee40b178c1812872
SvetlanaPetrova-s <petrovasp78@mail.ru>
2026-08-17 09:26:58 +0300

    Итоговая

--- blogicum/__init__.py
+++
@@ -1,8 +0,0 @@
-[pytest]
-DJANGO_SETTINGS_MODULE = blogicum.settings
-pythonpath = .
-norecursedirs = env/* venv/*
-addopts = -vv -p no:cacheprovider --disable-warnings
-testpaths = tests/
-python_files = test_*.py
-django_find_project = false
